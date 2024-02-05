643653e21d665878bb0f907c9334d9bfe4ea9c38
tr3atm3 <tttushar.789@gmail.com>
2024-02-05 23:36:44 +0530

    editButtonAdded

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function handleFormSubmit(event) {
   };
   const li = document.createElement("li");
   const del = document.createElement("button");
-  del.classname = "Delete";
+
   del.innerHTML = "Delete";
   del.addEventListener("click", function (event) {
     event.preventDefault();
@@ -22,10 +22,27 @@ function handleFormSubmit(event) {
     event.target.parentElement.style.display = "none";
     localStorage.removeItem(emailTo[1]);
   });
+  const edit = document.createElement("button");
+  edit.innerHTML = "Edit";
+  edit.addEventListener("click", function (event) {
+    event.preventDefault();
+    const emailTo =
+      event.target.parentElement.firstChild.textContent.split(" - ");
+    username.value = emailTo[0];
+    email.value = emailTo[1];
+    phone.value = emailTo[2];
+    event.target.parentElement.style.display = "none";
+    localStorage.removeItem(emailTo[1]);
+  });
   li.innerHTML = username.value + " - " + email.value + " - " + phone.value;
   li.appendChild(del);
+  li.appendChild(edit);
   ul.appendChild(li);
   localStorage.setItem(email.value, JSON.stringify(obj));
+
+  username.value = "";
+  email.value = "";
+  phone.value = "";
 }
 
 // const deletebtn = document.querySelector(".Delete");
